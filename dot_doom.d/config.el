b4606ac46e8fe86f575320ea1b684f49322e4a59
cubicme <seyedali.monemian@codesignal.com>
2023-05-05 12:51:05 +0400

    doom: windows resizing with hyrda

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -74,3 +74,21 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; Hydra
+(defhydra doom-window-resize-hydra (:hint nil)
+  "
+    _k_ increase height
+    _h_ decrease width
+    _l_ increase width
+    _j_ decrease height
+  "
+  ("h" evil-window-decrease-width)
+  ("j" evil-window-increase-height)
+  ("k" evil-window-decrease-height)
+  ("l" evil-window-increase-width)
+  ("q" nil))
+
+(map! :leader
+    (:prefix "w"
+      :desc "Hydra resize" :n "SPC" #'doom-window-resize-hydra/body))
